fc83fa72035a7ed310f00febde28ffc001688321
TimothyKariithi <timothy.kariithi@hgs.hiddengeniusproject.org>
2023-03-08 18:26:24 -0800

    first commit

---
+++ exerc1.py
@@ -0,0 +1,9 @@
+def multisum(num1, num2): #
+    if num1 * num2 <= 1000: # boolean data type; if number 1 multiplied by number 2 = less than 100 then print the product
+        return num1 * num2
+    
+    else:  # if the num1 and num2 multiplied are greater than 1000 then do not return and add them together.
+        return num1 + num2 
+    
+bob = multisum(37, 51) # variable
+print(bob)
